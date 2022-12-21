378dc45f158d810fbdff998478dfc11f6d0a301f
Elodie Daubie <elodie.daubie@gmail.com>
2022-12-21 16:57:46 +0100

    feat: create mutation CityUpdate

--- server/src/resolver/CityResolver.ts
+++ server/src/resolver/CityResolver.ts
@@ -1,4 +1,5 @@
 import { Arg, Mutation, Resolver, Query } from "type-graphql";
+import { ApolloError } from "apollo-server-errors";
 import City, { CityInput } from "../entity/City";
 import datasource from "../db";
 
@@ -13,8 +14,35 @@ export class CityResolver {
 
   @Query(() => [City])
   async getCities(): Promise<City[]> {
-    const cities = await datasource.getRepository(City).find();
-    console.log(cities);
-    return cities;
+    return await datasource.getRepository(City).find();
+    // console.log(cities);
+  }
+
+  @Mutation(() => City)
+  async updateCity(
+    @Arg("id") id: string,
+    @Arg("data") data: CityInput
+  ): Promise<City | undefined> {
+    try {
+      const { name, description, picture, latitude, longitude } = data;
+      const cityToUpdate = await datasource.getRepository(City).findOne({
+        where: { id: parseInt(id, 10) },
+      });
+
+      if (cityToUpdate === null)
+        throw new ApolloError("City not found", "NOT_FOUND");
+
+      cityToUpdate.name = name;
+      cityToUpdate.description = description;
+      cityToUpdate.picture = picture;
+      cityToUpdate.latitude = latitude;
+      cityToUpdate.longitude = longitude;
+
+      await datasource.getRepository(City).save(cityToUpdate);
+
+      return cityToUpdate;
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
