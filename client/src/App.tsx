64a68a92108bb2dd0985dc05d7be84431e2fd513
Alexandre Pillias <alexandre.pillias@gmail.com>
2022-12-21 12:00:40 +0100

    successful docker initialization with env variables

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-
 function App() {
   return (
     <>
       <Toaster position="bottom-center" />
       <main>
-        <Routes>
-        </Routes>
+        <p>Coucou !!</p>
+        <Routes></Routes>
       </main>
     </>
   );
