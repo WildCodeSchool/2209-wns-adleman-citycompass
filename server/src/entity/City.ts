a99c523ef6a7cdf9dcac0e9a24f528f4b320f5a9
Elodie Daubie <elodie.daubie@gmail.com>
2022-12-22 11:37:02 +0100

    feat : add min lenght to InputType

--- server/src/entity/City.ts
+++ server/src/entity/City.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { Field, InputType, ObjectType } from "type-graphql";
-import { MaxLength } from "class-validator";
+import { MaxLength, MinLength } from "class-validator";
 
 @Entity()
 @ObjectType()
@@ -34,21 +34,26 @@ class City {
 export class CityInput {
   @Field()
   @MaxLength(50)
+  @MinLength(2)
   name: string;
 
   @Field()
   @MaxLength(2083)
+  @MinLength(21)
   picture: string;
 
   @Field()
+  @MinLength(10)
   description: string;
 
   @Field()
   @MaxLength(12)
+  @MinLength(1)
   latitude: string;
 
   @Field()
   @MaxLength(13)
+  @MinLength(1)
   longitude: string;
 }
 export default City;
