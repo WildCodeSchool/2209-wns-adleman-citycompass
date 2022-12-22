f9c7b3e75d05ce9dd92f98add8ea0c678042544d
Julie Combeau <combeau.julie@gmail.com>
2022-12-22 10:56:59 +0100

    add routing system

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import Home from "./pages/Home";
+import City from "./pages/City";
+import Place from "./pages/Place";
+import Account from "./pages/Account";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <>
       <Toaster position="bottom-center" />
       <main>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cities/:cityId" element={<City />} />
+          <Route path="/places/:placeId" element={<Place />} />
+          <Route path="/account/:userId" element={<Account />} />
+          <Route path="/dashboard/:userId" element={<Dashboard />} />
+        </Routes>
       </main>
     </>
   );
