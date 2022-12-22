89747d8bcb5efde37b9ccad674030fd05345f65f
Alexandre Pillias <alexandre.pillias@gmail.com>
2022-12-22 17:16:59 +0100

    first tests written 1 passed ans 1 failed

--- integration-tests/setupTests.ts
+++ integration-tests/setupTests.ts
@@ -1,26 +1,25 @@
-// import db from "../server/src/db";
+import db from "../server/src/db";
 
 // https://blog.tooljet.com/clearing-tables-before-each-test-nestjs-typeorm/
 // https://github.com/typeorm/typeorm/issues/2978#issuecomment-730596460
-// async function clearDB() {
-//   const entities = db.entityMetadatas;
-//   return Promise.all(
-//     entities.map((entity) => db.getRepository(entity.name).delete({}))
-//   );
-// }
+async function clearDB() {
+  const entities = db.entityMetadatas;
+  return Promise.all(
+    entities.map((entity) => db.getRepository(entity.name).delete({}))
+  );
+}
 
-// TODO: init db before all tests
-// TODO: clear db before each tests
-// TODO: close db connection after all tests
+// init db before all tests
+beforeAll(async () => {
+  await db.initialize();
+});
 
-// beforeAll(async () => {
-//   await db.initialize();
-// });
+// clear db before each tests
+beforeEach(async () => {
+  await clearDB();
+});
 
-// beforeEach(async () => {
-//   await clearDB();
-// });
-
-// afterAll(async () => {
-//   await db.destroy();
-// });
+// close db connection after all tests
+afterAll(async () => {
+  await db.destroy();
+});
