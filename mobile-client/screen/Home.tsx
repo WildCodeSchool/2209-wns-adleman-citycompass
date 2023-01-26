dc9a3e18125a78b37412a0d709109e229983b6fd
Elodie Daubie <elodie.daubie@gmail.com>
2023-01-26 11:23:52 +0100

    feat: install navigation and place details view

---
+++ mobile-client/screen/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+
+// Type any is from documentation
+export default function Home({ navigation }: any) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Place-details")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ mobile-client/screen/PlaceDetails.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function PlaceDetails() {
+  return (
+    <View>
+      <Text> Hello Details </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
