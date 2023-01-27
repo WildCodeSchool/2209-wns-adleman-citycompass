eb34948ae72b5261d546f94ea5365834df973579
Elodie Daubie <elodie.daubie@gmail.com>
2023-01-27 12:19:15 +0100

    merge of home and poi detail and some style

--- mobile-client/gql/generated/schema.ts
+++ mobile-client/gql/generated/schema.ts
@@ -1,16 +1,10 @@
-import { gql } from "@apollo/client";
-import * as Apollo from "@apollo/client";
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]?: Maybe<T[SubKey]>;
-};
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]: Maybe<T[SubKey]>;
-};
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
@@ -22,37 +16,37 @@ export type Scalars = {
 };
 
 export type Category = {
-  __typename?: "Category";
-  id: Scalars["Float"];
-  name: Scalars["String"];
-  picto: Scalars["String"];
+  __typename?: 'Category';
+  id: Scalars['Float'];
+  name: Scalars['String'];
+  picto: Scalars['String'];
 };
 
 export type CategoryInput = {
-  name: Scalars["String"];
-  picto: Scalars["String"];
+  name: Scalars['String'];
+  picto: Scalars['String'];
 };
 
 export type City = {
-  __typename?: "City";
-  description: Scalars["String"];
-  id: Scalars["Float"];
-  latitude: Scalars["String"];
-  longitude: Scalars["String"];
-  name: Scalars["String"];
-  picture: Scalars["String"];
+  __typename?: 'City';
+  description: Scalars['String'];
+  id: Scalars['Float'];
+  latitude: Scalars['String'];
+  longitude: Scalars['String'];
+  name: Scalars['String'];
+  picture: Scalars['String'];
 };
 
 export type CityInput = {
-  description: Scalars["String"];
-  latitude: Scalars["String"];
-  longitude: Scalars["String"];
-  name: Scalars["String"];
-  picture: Scalars["String"];
+  description: Scalars['String'];
+  latitude: Scalars['String'];
+  longitude: Scalars['String'];
+  name: Scalars['String'];
+  picture: Scalars['String'];
 };
 
 export type Mutation = {
-  __typename?: "Mutation";
+  __typename?: 'Mutation';
   createCategory: Category;
   createCity: City;
   createPlace: Place;
@@ -61,61 +55,67 @@ export type Mutation = {
   updatePlace: Place;
 };
 
+
 export type MutationCreateCategoryArgs = {
   data: CategoryInput;
 };
+
 
 export type MutationCreateCityArgs = {
   data: CityInput;
 };
 
+
 export type MutationCreatePlaceArgs = {
   data: PlaceInput;
 };
 
+
 export type MutationUpdateCategoryArgs = {
   data: CategoryInput;
-  id: Scalars["Int"];
+  id: Scalars['Int'];
 };
+
 
 export type MutationUpdateCityArgs = {
   data: CityInput;
-  id: Scalars["String"];
+  id: Scalars['String'];
 };
+
 
 export type MutationUpdatePlaceArgs = {
   data: PlaceInput;
-  id: Scalars["String"];
+  id: Scalars['String'];
 };
 
 export type Place = {
-  __typename?: "Place";
-  adress: Scalars["String"];
-  categoryId: Scalars["Float"];
-  cityId: Scalars["Float"];
-  description: Scalars["String"];
-  id: Scalars["Float"];
-  latitude: Scalars["String"];
-  longitude: Scalars["String"];
-  name: Scalars["String"];
-  picture: Scalars["String"];
-  website?: Maybe<Scalars["String"]>;
+  __typename?: 'Place';
+  adress: Scalars['String'];
+  categoryId: Scalars['Float'];
+  cityId: Scalars['Float'];
+  description: Scalars['String'];
+  id: Scalars['Float'];
+  latitude: Scalars['String'];
+  longitude: Scalars['String'];
+  name: Scalars['String'];
+  picture: Scalars['String'];
+  website?: Maybe<Scalars['String']>;
 };
 
 export type PlaceInput = {
-  adress: Scalars["String"];
-  categoryId: Scalars["Float"];
-  cityId: Scalars["Float"];
-  description: Scalars["String"];
-  latitude: Scalars["String"];
-  longitude: Scalars["String"];
-  name: Scalars["String"];
-  picture: Scalars["String"];
-  website?: InputMaybe<Scalars["String"]>;
+  adress: Scalars['String'];
+  categoryId: Scalars['Float'];
+  cityId: Scalars['Float'];
+  description: Scalars['String'];
+  latitude: Scalars['String'];
+  longitude: Scalars['String'];
+  name: Scalars['String'];
+  picture: Scalars['String'];
+  website?: InputMaybe<Scalars['String']>;
 };
 
 export type Query = {
-  __typename?: "Query";
+  __typename?: 'Query';
   getCategories: Array<Category>;
   getCities: Array<City>;
   getOneCitybyId: City;
@@ -123,65 +123,41 @@ export type Query = {
   getPlaces: Array<Place>;
 };
 
+
 export type QueryGetOneCitybyIdArgs = {
-  id: Scalars["String"];
+  id: Scalars['String'];
 };
+
 
 export type QueryGetOnePlacebyIdArgs = {
-  id: Scalars["String"];
+  id: Scalars['String'];
 };
 
-export type GetPlacesQueryVariables = Exact<{ [key: string]: never }>;
 export type GetOnePlacebyIdQueryVariables = Exact<{
-  getOnePlacebyIdId: Scalars["String"];
+  getOnePlacebyIdId: Scalars['String'];
 }>;
 
-export type GetOnePlacebyIdQuery = {
-  __typename?: "Query";
-  getOnePlacebyId: {
-    __typename?: "Place";
-    id: number;
-    name: string;
-    adress: string;
-    website?: string | null;
-    picture: string;
-    description: string;
-  };
-};
 
-export type GetPlacesQuery = {
-  __typename?: "Query";
-  getPlaces: Array<{
-    __typename?: "Place";
-    name: string;
-    id: number;
-    cityId: number;
-    picture: string;
-  }>;
-};
+export type GetOnePlacebyIdQuery = { __typename?: 'Query', getOnePlacebyId: { __typename?: 'Place', id: number, name: string, adress: string, website?: string | null, picture: string, description: string } };
 
-export const GetPlacesDocument = gql`
-  query GetPlaces {
-    getPlaces {
-      name
-      id
-      cityId
-      picture
-    }
-  }
-`;
+export type GetPlacesQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetPlacesQuery = { __typename?: 'Query', getPlaces: Array<{ __typename?: 'Place', name: string, id: number, cityId: number, picture: string }> };
+
+
 export const GetOnePlacebyIdDocument = gql`
-  query GetOnePlacebyId($getOnePlacebyIdId: String!) {
-    getOnePlacebyId(id: $getOnePlacebyIdId) {
-      id
-      name
-      adress
-      website
-      picture
-      description
-    }
+    query GetOnePlacebyId($getOnePlacebyIdId: String!) {
+  getOnePlacebyId(id: $getOnePlacebyIdId) {
+    id
+    name
+    adress
+    website
+    picture
+    description
   }
-`;
+}
+    `;
 
 /**
  * __useGetOnePlacebyIdQuery__
@@ -199,66 +175,51 @@ export const GetOnePlacebyIdDocument = gql`
  *   },
  * });
  */
-export function useGetPlacesQuery(
-  baseOptions?: Apollo.QueryHookOptions<GetPlacesQuery, GetPlacesQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useQuery<GetPlacesQuery, GetPlacesQueryVariables>(
-    GetPlacesDocument,
-    options
-  );
+export function useGetOnePlacebyIdQuery(baseOptions: Apollo.QueryHookOptions<GetOnePlacebyIdQuery, GetOnePlacebyIdQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetOnePlacebyIdQuery, GetOnePlacebyIdQueryVariables>(GetOnePlacebyIdDocument, options);
+      }
+export function useGetOnePlacebyIdLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetOnePlacebyIdQuery, GetOnePlacebyIdQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetOnePlacebyIdQuery, GetOnePlacebyIdQueryVariables>(GetOnePlacebyIdDocument, options);
+        }
+export type GetOnePlacebyIdQueryHookResult = ReturnType<typeof useGetOnePlacebyIdQuery>;
+export type GetOnePlacebyIdLazyQueryHookResult = ReturnType<typeof useGetOnePlacebyIdLazyQuery>;
+export type GetOnePlacebyIdQueryResult = Apollo.QueryResult<GetOnePlacebyIdQuery, GetOnePlacebyIdQueryVariables>;
+export const GetPlacesDocument = gql`
+    query GetPlaces {
+  getPlaces {
+    name
+    id
+    cityId
+    picture
+  }
 }
-export function useGetPlacesLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<
-    GetPlacesQuery,
-    GetPlacesQueryVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useLazyQuery<GetPlacesQuery, GetPlacesQueryVariables>(
-    GetPlacesDocument,
-    options
-  );
-}
+    `;
+
+/**
+ * __useGetPlacesQuery__
+ *
+ * To run a query within a React component, call `useGetPlacesQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetPlacesQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetPlacesQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetPlacesQuery(baseOptions?: Apollo.QueryHookOptions<GetPlacesQuery, GetPlacesQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetPlacesQuery, GetPlacesQueryVariables>(GetPlacesDocument, options);
+      }
+export function useGetPlacesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetPlacesQuery, GetPlacesQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetPlacesQuery, GetPlacesQueryVariables>(GetPlacesDocument, options);
+        }
 export type GetPlacesQueryHookResult = ReturnType<typeof useGetPlacesQuery>;
-export type GetPlacesLazyQueryHookResult = ReturnType<
-  typeof useGetPlacesLazyQuery
->;
-export type GetPlacesQueryResult = Apollo.QueryResult<
-  GetPlacesQuery,
-  GetPlacesQueryVariables
->;
-export function useGetOnePlacebyIdQuery(
-  baseOptions: Apollo.QueryHookOptions<
-    GetOnePlacebyIdQuery,
-    GetOnePlacebyIdQueryVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useQuery<GetOnePlacebyIdQuery, GetOnePlacebyIdQueryVariables>(
-    GetOnePlacebyIdDocument,
-    options
-  );
-}
-export function useGetOnePlacebyIdLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<
-    GetOnePlacebyIdQuery,
-    GetOnePlacebyIdQueryVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useLazyQuery<
-    GetOnePlacebyIdQuery,
-    GetOnePlacebyIdQueryVariables
-  >(GetOnePlacebyIdDocument, options);
-}
-export type GetOnePlacebyIdQueryHookResult = ReturnType<
-  typeof useGetOnePlacebyIdQuery
->;
-export type GetOnePlacebyIdLazyQueryHookResult = ReturnType<
-  typeof useGetOnePlacebyIdLazyQuery
->;
-export type GetOnePlacebyIdQueryResult = Apollo.QueryResult<
-  GetOnePlacebyIdQuery,
-  GetOnePlacebyIdQueryVariables
->;
+export type GetPlacesLazyQueryHookResult = ReturnType<typeof useGetPlacesLazyQuery>;
+export type GetPlacesQueryResult = Apollo.QueryResult<GetPlacesQuery, GetPlacesQueryVariables>;
--- mobile-client/screens/PlaceDetails.tsx
+++ mobile-client/screens/PlaceDetails.tsx
@@ -1,7 +1,95 @@
 import React from "react";
+import { StyleSheet, View, Text, Image, ScrollView } from "react-native";
+import { useGetOnePlacebyIdQuery } from "../gql/generated/schema";
 
-function PlaceDetails() {
-	return <div>PlaceDetails</div>;
+export default function PlaceDetails({ route }: any) {
+  const { itemId } = route.params;
+
+  const { data } = useGetOnePlacebyIdQuery({
+    variables: { getOnePlacebyIdId: itemId.toString() },
+  });
+
+  const place = data?.getOnePlacebyId;
+
+  return (
+    <ScrollView style={styles.container}>
+      {/* first section : presentation */}
+      <View style={styles.firstSection}>
+        <Image source={{ uri: place?.picture }} style={styles.image} />
+        <Text style={styles.title1}>{place?.name}</Text>
+        {/* adress block */}
+        <View style={styles.innerBlock}>
+          <Image
+            source={require("../assets/address-icon-mobile.png")}
+            style={styles.icon}
+          />
+          <Text>{place?.adress}</Text>
+        </View>
+        {/* website block */}
+        <View style={styles.innerBlock}>
+          <Image
+            source={require("../assets/website-icon-mobile.png")}
+            style={styles.icon}
+          />
+          <Text>{place?.website}</Text>
+        </View>
+      </View>
+
+      {/* second section : description */}
+      <View style={styles.secondSection}>
+        <Text style={styles.title2}>Description</Text>
+        <Text style={styles.description}>{place?.description}</Text>
+      </View>
+    </ScrollView>
+  );
 }
 
-export default PlaceDetails;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  image: {
+    height: 300,
+  },
+  firstSection: {
+    flex: 2,
+    justifyContent: "space-between",
+    marginBottom: 30,
+  },
+  innerBlock: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  icon: {
+    marginRight: 10,
+  },
+  title1: {
+    fontSize: 40,
+    textTransform: "uppercase",
+    fontFamily: "Lato-Black",
+    textAlign: "center",
+    paddingVertical: 20,
+    textShadowColor: "#F6CDAF",
+    textShadowOffset: { width: 5, height: 5 },
+    textShadowRadius: 1,
+  },
+  secondSection: {
+    flex: 1,
+  },
+  title2: {
+    color: "#23272D",
+    fontSize: 25,
+    fontWeight: "500",
+    textAlign: "center",
+    fontFamily: "Lato-Black",
+  },
+  description: {
+    color: "#23272D",
+    fontFamily: "Karla-Medium",
+    padding: 20,
+    lineHeight: 25,
+  },
+});
