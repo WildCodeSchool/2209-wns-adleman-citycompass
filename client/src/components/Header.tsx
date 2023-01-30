f5a135be85d517c4a1dddcd3d613f41633fb04db
Alexandre Pillias <alexandre.pillias@gmail.com>
2023-01-30 12:20:01 +0100

    started working on header + familiarizing with tailiwind

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <div className="header_container header_container_flex">
+      <div>ICONS</div> <div>CITY COMPASS</div> <div>PROFILE + SEARCH BAR</div>
+    </div>
+  );
+}
+
+export default Header;
