db6e212fdbe4eccdfc623369007d5b9564ddc6f3
Elodie Daubie <elodie.daubie@gmail.com>
2023-01-30 12:30:00 +0100

    style: add font size to tailwind.config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -15,6 +15,42 @@ module.exports = {
         darkBlue: "#23272D",
         red: "#E91616",
       },
+      fontFace: {
+        karla: [
+          {
+            filename: "./assets/fonts/karla/Karla-Medium",
+            weigth: 400,
+          },
+        ],
+        latoBlack: [
+          {
+            filename: "./assets/fonts/lato/Lato-Black",
+            weigth: 900,
+          },
+        ],
+        latoRegular: [
+          {
+            filename: "./assets/fonts/lato/Lato-Regular",
+            weigth: 400,
+          },
+        ],
+      },
+      fontFamily: {
+        karla: ["karla", "sans-serif"],
+        latoBlack: ["latoBlack", "system-ui"],
+        latoRegular: ["latoRegular", "sans-serif"],
+      },
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        m: "2rem",
+        l: "2.5rem",
+        xl: "4rem",
+        "2xl": "1.563rem",
+        "3xl": "1.953rem",
+        "4xl": "2.441rem",
+        "5xl": "3.052rem",
+      },
     },
   },
   plugins: [],
