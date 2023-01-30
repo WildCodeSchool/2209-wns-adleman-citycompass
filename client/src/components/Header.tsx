4848d2402674254aa7b71a3d8d809850aba1ea0e
Alexandre Pillias <alexandre.pillias@gmail.com>
2023-01-30 17:17:23 +0100

    added new icons and fixed flex

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "../styles/Header.css";
+import compass from "../assets/compass.png";
+import loupe from "../assets/magnifying-glass.svg";
+import person from "../assets/person-circle-outline.svg";
 
 function Header() {
   return (
-    <div className="header_container header_container_flex">
-      <div>ICONS</div> <div>CITY COMPASS</div> <div>PROFILE + SEARCH BAR</div>
+    <div className="header_container header_container_flex flex">
+      <div className="header_compass">
+        <img src={compass} alt="compass" className="header_compass" />
+      </div>{" "}
+      <div className="header_title">CITY COMPASS</div>
+      <div className="header_profile_search">
+        <img src={loupe} alt="loupe" className="header_glass_img" />
+        <img src={person} alt="person" className="header_glass_img" />
+      </div>
     </div>
   );
 }
