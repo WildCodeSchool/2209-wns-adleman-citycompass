ff68115249c015feaa3d69f362e5ee298eb0be6c
Julie Combeau <combeau.julie@gmail.com>
2023-01-31 15:23:47 +0100

    design footer finalised

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -11,75 +11,81 @@ import linkedinLogo from "../assets/linkedin-logo.svg";
 
 export default function Footer() {
   return (
-    <div className="footer-container">
-      <div className="logo-container">
-        <img
-          className="w-16 lg:w-20"
-          src={compasLogo}
-          alt="logo city compass"
-        />
-        <p>City Compass</p>
-      </div>
-      <div className="contributors-container">
-        <p>Made with love by</p>
-        <div className="flex space-x-4">
-          <a href="https://github.com/Liax" rel="noreferrer" target="_blank">
-            <img
-              className="avatar-footer"
-              src={claraContributor}
-              alt="clara avatar"
-            />
-          </a>
-          <a
-            href="https://github.com/elodiedaubie"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img
-              className="avatar-footer"
-              src={elodieContributor}
-              alt="élodie avatar"
-            />
-          </a>
-          <a
-            href="https://github.com/kaitolight"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img
-              className="avatar-footer"
-              src={alexandreContributor}
-              alt="alexandre avatar"
-            />
-          </a>
-          <a
-            href="https://github.com/JulieCombeau"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img
-              className="avatar-footer"
-              src={julieContributor}
-              alt="julie avatar"
-            />
-          </a>
+    <footer className="bg-[theme('colors.green.DEFAULT')]">
+      <div className="footer-container">
+        <div className="logo-container">
+          <img
+            className="w-16 lg:w-20"
+            src={compasLogo}
+            alt="logo city compass"
+          />
+          <h2 className="drop-shadow-[0.2rem_0.1rem_0_theme('colors.cream')] w-44">
+            City Compass
+          </h2>
+        </div>
+        <div className="contributors-container">
+          <p className="font-latoRegular">Made with love by</p>
+          <div className="flex space-x-4">
+            <a href="https://github.com/Liax" rel="noreferrer" target="_blank">
+              <img
+                className="avatar-footer"
+                src={claraContributor}
+                alt="clara avatar"
+              />
+            </a>
+            <a
+              href="https://github.com/elodiedaubie"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img
+                className="avatar-footer"
+                src={elodieContributor}
+                alt="élodie avatar"
+              />
+            </a>
+            <a
+              href="https://github.com/kaitolight"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img
+                className="avatar-footer"
+                src={alexandreContributor}
+                alt="alexandre avatar"
+              />
+            </a>
+            <a
+              href="https://github.com/JulieCombeau"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img
+                className="avatar-footer"
+                src={julieContributor}
+                alt="julie avatar"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="reseaux-container">
+          <p className="text-2xl font-bold font-latoBlack md:w-20">
+            Suivez nous
+          </p>
+          <div className="reseau-footer">
+            <img src={instagramLogo} alt="instagram logo" />
+            <img src={facebookLogo} alt="facebook logo" />
+            <a
+              href="https://github.com/WildCodeSchool/2209-wns-adleman-citycompass"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img src={githubLogo} alt="gitHub logo" />
+            </a>
+            <img src={linkedinLogo} alt="linkedIn logo" />
+          </div>
         </div>
       </div>
-      <div className="reseaux-container">
-        <p>Suivez-nous</p>
-        <div className="reseau-footer">
-          <img src={instagramLogo} alt="instagram logo" />
-          <img src={facebookLogo} alt="facebook logo" />
-          <a
-            href="https://github.com/WildCodeSchool/2209-wns-adleman-citycompass"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img src={githubLogo} alt="gitHub logo" />
-          </a>
-          <img src={linkedinLogo} alt="linkedIn logo" />
-        </div>
-      </div>
-    </div>
+    </footer>
   );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,9 @@ import Footer from "../components/Footer";
 
 export default function Home() {
   return (
+    // Container parent à definir : pour l'instant cela force le footer à être en bas de page peut importe le contenu
     <div className="flex h-full flex-col justify-between">
+      {/*  */}
       <div className="h-96 w-96 rounded shadow m-12">
         <h1 className=" drop-shadow-xl">Titre 1</h1>
         <h2 className="drop">Titre 2</h2>
