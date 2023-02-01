136be2d2308eac879ce862fe352e980603cbbebd
Clara Martin <clara@studiometa.fr>
2023-02-01 16:59:19 +0100

    Create a hero componant and add it to the city page

---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function Hero({ heroContent, breadcrumbs }: any) {
+	return (
+		heroContent && (
+			<div className="hero sm:h-screen flex flex-col sm:flex-row justify-start bg-cream">
+				<div className="w-full sm:w-1/2 overflow-hidden">
+					<img
+						className="w-full sm:w-auto sm:max-w-none h-full"
+						src={heroContent.picture}
+						alt={heroContent.name + "picture"}
+					/>
+				</div>
+				<div className="w-full sm:w-1/2 px-6 sm:pr-9 flex flex-col justify-center sm:space-y-8 max-w-2xl">
+					<div className="breadcrumbs hidden sm:flex">
+						{breadcrumbs && (
+							<div className="max-w-3xl">
+								{breadcrumbs.map((breadcrumb: any) => (
+									<a
+										href={breadcrumb.link}
+										className="text-xs text-darkBlue/30"
+									>
+										<span>{breadcrumb.name}</span>
+									</a>
+								))}
+							</div>
+						)}
+					</div>
+					<h1 className="drop-shadow-xl text-center sm:text-left">
+						{heroContent.name}
+					</h1>
+					<p className="hidden sm:inline-block">{heroContent.description}</p>
+				</div>
+			</div>
+		)
+	);
+}
+
+export default Hero;
--- client/src/pages/City.tsx
+++ client/src/pages/City.tsx
@@ -1,10 +1,26 @@
 import MapCity from "../components/MapCity";
+import Hero from "../components/Hero";
+import { useGetOneCitybyIdQuery } from "../gql/generated/schema";
 
 export default function City() {
-  return (
-    <div>
-      <p> City</p>
-      <MapCity />
-    </div>
-  );
+	const id = "14";
+	const { data } = useGetOneCitybyIdQuery({
+		variables: { getOneCitybyId: id },
+	});
+
+	const city = data?.getOneCitybyId;
+
+	//https://medium.com/@mattywilliams/generating-an-automatic-breadcrumb-in-react-router-fed01af1fc3
+	const breadcrumbs = [
+		{ name: "Accueil", link: "/" },
+		{ name: "cities", link: "" },
+		{ name: " Lyon", link: "" },
+	];
+
+	return (
+		<>
+			<Hero heroContent={city} breadcrumbs={breadcrumbs} />
+			<MapCity />
+		</>
+	);
 }
