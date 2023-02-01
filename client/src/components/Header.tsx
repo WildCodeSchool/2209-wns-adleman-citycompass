a7b0b04efc99c230c20301052cd660bf5596bba0
Alexandre Pillias <alexandre.pillias@gmail.com>
2023-02-01 16:47:07 +0100

    header for large screen

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "../styles/Header.css";
-import compass from "../assets/compass.png";
+import compasLogo from "../assets/compas_logo.svg";
 import loupe from "../assets/magnifying-glass.svg";
 import person from "../assets/person-circle-outline.svg";
 
 function Header() {
   return (
-    <div className="header_container header_container_flex flex">
+    <div className="header_container">
       <div className="header_compass">
-        <img src={compass} alt="compass" className="header_compass" />
+        <img src={compasLogo} alt="compass" className="header_compass" />
       </div>{" "}
-      <div className="header_title">CITY COMPASS</div>
+      <h1 className="header_title">CITY COMPASS</h1>
       <div className="header_profile_search">
         <img src={loupe} alt="loupe" className="header_glass_img" />
-        <img src={person} alt="person" className="header_glass_img" />
+        <img src={person} alt="person" className="header_profile_img" />
       </div>
     </div>
   );
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,17 +1,22 @@
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    // Container parent à definir : pour l'instant cela force le footer à être en bas de page peut importe le contenu
-    <div className="flex h-full flex-col justify-between">
-      <div className="h-96 w-96 rounded shadow m-12">
-        <h1 className=" drop-shadow-xl">Titre 1</h1>
-        <h2 className="drop">Titre 2</h2>
-        <h3>Titre 3</h3>
-        <h4>Titre 4</h4>
-        <p className="">Mon super test</p>
+    <>
+      <Header />
+      {/* Container parent à definir : pour l'instant cela force le footer à être
+      en bas de page peut importe le contenu*/}
+      <div className="flex h-full flex-col justify-between">
+        <div className="h-96 w-96 rounded shadow m-12">
+          <h1 className=" drop-shadow-xl">Titre 1</h1>
+          <h2 className="drop">Titre 2</h2>
+          <h3>Titre 3</h3>
+          <h4>Titre 4</h4>
+          <p className="">Mon super test</p>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 }
