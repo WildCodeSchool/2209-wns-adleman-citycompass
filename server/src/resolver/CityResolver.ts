69ff2715ecd2d3a87c94083daf9212b77425c928
Clara Martin <clara@studiometa.fr>
2023-02-01 10:53:35 +0100

    Update the resolvers to have the categories relations

--- server/src/resolver/CityResolver.ts
+++ server/src/resolver/CityResolver.ts
@@ -6,61 +6,62 @@ import { existingCity, existingCoordinates } from "../helpers/dbCheckers";
 
 @Resolver(City)
 export class CityResolver {
-  @Mutation(() => City)
-  async createCity(@Arg("data") data: CityInput): Promise<City> {
-    if (data === null)
-      throw new ApolloError("No data in query", "BAD_USER_INPUT");
+	@Mutation(() => City)
+	async createCity(@Arg("data") data: CityInput): Promise<City> {
+		if (data === null)
+			throw new ApolloError("No data in query", "BAD_USER_INPUT");
 
-    // check if city name & coordinates are already in database
-    await existingCity(data);
-    await existingCoordinates(data);
+		// check if city name & coordinates are already in database
+		await existingCity(data);
+		await existingCoordinates(data);
 
-    return await datasource.getRepository(City).save(data);
-  }
+		return await datasource.getRepository(City).save(data);
+	}
 
-  @Query(() => [City])
-  async getCities(): Promise<City[]> {
-    return await datasource.getRepository(City).find();
-  }
+	@Query(() => [City])
+	async getCities(): Promise<City[]> {
+		return await datasource.getRepository(City).find();
+	}
 
-  @Mutation(() => City)
-  async updateCity(
-    @Arg("id") id: string,
-    @Arg("data") data: CityInput
-  ): Promise<City | undefined> {
-    const { name, description, picture, latitude, longitude } = data;
+	@Mutation(() => City)
+	async updateCity(
+		@Arg("id") id: string,
+		@Arg("data") data: CityInput
+	): Promise<City | undefined> {
+		const { name, description, picture, latitude, longitude } = data;
 
-    const cityToUpdate = await datasource.getRepository(City).findOne({
-      where: { id: parseInt(id, 10) },
-    });
+		const cityToUpdate = await datasource.getRepository(City).findOne({
+			where: { id: parseInt(id, 10) },
+		});
 
-    if (cityToUpdate === null)
-      throw new ApolloError("City not found", "NOT_FOUND");
+		if (cityToUpdate === null)
+			throw new ApolloError("City not found", "NOT_FOUND");
 
-    // check if city name & coordinates are already in database
-    await existingCity(data, id);
-    await existingCoordinates(data);
+		// check if city name & coordinates are already in database
+		await existingCity(data, id);
+		await existingCoordinates(data);
 
-    cityToUpdate.name = name;
-    cityToUpdate.description = description;
-    cityToUpdate.picture = picture;
-    cityToUpdate.latitude = latitude;
-    cityToUpdate.longitude = longitude;
+		cityToUpdate.name = name;
+		cityToUpdate.description = description;
+		cityToUpdate.picture = picture;
+		cityToUpdate.latitude = latitude;
+		cityToUpdate.longitude = longitude;
 
-    await datasource.getRepository(City).save(cityToUpdate);
+		await datasource.getRepository(City).save(cityToUpdate);
 
-    return cityToUpdate;
-  }
+		return cityToUpdate;
+	}
 
-  @Query(() => City)
-  async getOneCitybyId(@Arg("id") id: string): Promise<City> {
-    const cityToFind = await datasource
-      .getRepository(City)
-      .findOne({ where: { id: parseInt(id, 10) } });
+	@Query(() => City)
+	async getOneCitybyId(@Arg("id") id: string): Promise<City> {
+		const cityToFind = await datasource.getRepository(City).findOne({
+			where: { id: parseInt(id, 10) },
+			relations: { places: { category: true } },
+		});
 
-    if (cityToFind === null)
-      throw new ApolloError("City not found", "NOT_FOUND");
+		if (cityToFind === null)
+			throw new ApolloError("City not found", "NOT_FOUND");
 
-    return cityToFind;
-  }
+		return cityToFind;
+	}
 }
