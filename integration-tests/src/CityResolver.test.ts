4fcad533411812c5ffa4e6e5c03b97d6b8b98883
Elodie Daubie <elodie.daubie@gmail.com>
2023-02-03 11:47:00 +0100

    rebase : resolve conflict with dev

--- integration-tests/src/CityResolver.test.ts
+++ integration-tests/src/CityResolver.test.ts
@@ -39,6 +39,7 @@ const getCityQuery = gql`
 
 describe("City resolver", () => {
   describe("create city", () => {
+    //a city with valid paramters should be created
     it("should create city with valid parameters", async () => {
       const res = await client.mutate({
         mutation: createCityMutation,
@@ -62,6 +63,67 @@ describe("City resolver", () => {
       expect(res.data?.createCity).toHaveProperty("latitude", "45.764043");
       expect(res.data?.createCity).toHaveProperty("longitude", "4.835659");
     });
+    // a city should start with a capital letter
+    it("should not create city with no capital letter", async () => {
+      const res = await client.mutate({
+        mutation: createCityMutation,
+        variables: {
+          data: {
+            name: "paris",
+            description: "La description de Paris",
+            picture: "https://picsum.photos/200/300",
+            longitude: "2.3522219",
+            latitude: "48.856614",
+          },
+        },
+      });
+
+      expect(res.data?.createCity).toHaveProperty("id");
+      expect(res.data?.createCity).toHaveProperty("name", "Paris");
+      expect(res.data?.createCity).toHaveProperty(
+        "description",
+        "La description de Paris"
+      );
+      expect(res.data?.createCity).toHaveProperty("latitude", "48.856614");
+      expect(res.data?.createCity).toHaveProperty("longitude", "2.3522219");
+    });
+    //a city should not start with a space
+    it("a city should not start with a space", async () => {
+      const res = await client.mutate({
+        mutation: createCityMutation,
+        variables: {
+          data: {
+            name: " Marseille",
+            description: "La description de Marseille",
+            picture: "https://picsum.photos/200/300",
+            longitude: "5.400000",
+            latitude: "43.300000",
+          },
+        },
+      });
+
+      expect(res.data?.createCity).toHaveProperty("id");
+      expect(res.data?.createCity).toHaveProperty("name", " Marseille");
+      expect(res.data?.createCity).toHaveProperty(
+        "description",
+        "La description de Marseille"
+      );
+      expect(res.data?.createCity).toHaveProperty("latitude", "43.300000");
+      expect(res.data?.createCity).toHaveProperty("longitude", "5.400000");
+    });
+    // a city name should not be created twice
+    // a city with same latitude and longitude should not be created twice
+    // a city with empty name should not be created
+    // a city with empty description should not be created
+    // a city with empty picture should not be created
+    // a city with empty longitude should not be created
+    // a city with empty latitude should not be created
+    // a picture should be only URLs
+    // unvalid longitude should not be accepted (ex : other caracters than 0 to 9)
+    // unvalid latitude should not be accepted (ex : other caracters than 0 to 9)
+    // city descriptions with less than 10 letters should not be accepted
+    // descriptions with SQL requests should not have consequences
+    // descriptions with JS scripts should not have consequences
   });
 
   describe("read cities", () => {
