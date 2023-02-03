7e406ea366a7c63ddb4701175f87d2c9274485e7
Clara Martin <clara@studiometa.fr>
2023-02-03 10:04:40 +0100

    Fix the bg on the header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -7,26 +7,28 @@ import person from "../assets/person-circle-outline.svg";
 function Header() {
   return (
     <>
-      <div className="container header">
-        <div className="header__compass">
-          <img
-            src={compasHeader}
-            alt="logo city compass"
-            className="header__compass-img"
-          />
-        </div>
-        <h1 className="header__title">CITY COMPASS</h1>
-        <div className="header__profile">
-          <img
-            src={loupe}
-            alt="loupe search bar"
-            className="header__profile--loupe"
-          />
-          <img
-            src={person}
-            alt="person icon to profile"
-            className="header__profile--search"
-          />
+      <div className="bg-white w-full">
+        <div className="header container">
+          <div className="header__compass">
+            <img
+              src={compasHeader}
+              alt="logo city compass"
+              className="header__compass-img"
+            />
+          </div>
+          <h1 className="header__title">CITY COMPASS</h1>
+          <div className="header__profile">
+            <img
+              src={loupe}
+              alt="loupe search bar"
+              className="header__profile--loupe"
+            />
+            <img
+              src={person}
+              alt="person icon to profile"
+              className="header__profile--search"
+            />
+          </div>
         </div>
       </div>
     </>
