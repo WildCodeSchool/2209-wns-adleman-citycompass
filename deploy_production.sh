e99b227fd8e500b7c32ab147e8b5bddb046a95ef
Julie Combeau <combeau.julie@gmail.com>
2023-03-29 17:11:46 +0200

    update docker production

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,5 +1,5 @@
-#!/bi
+#!/bin/sh
 git fetch origin && git reset --hard origin/main && git clean -f -d && \
 docker compose -f docker-compose.production.yml down && \
-docker compose -f docker-compose.production.yml pull && \
-docker compose -f docker-compose.production.yml --env-file .env.production up -d;
+    docker compose -f docker-compose.production.yml pull && \
+    GATEWAY_PORT=8000 docker compose -f docker-compose.production.yml --env-file .env.production up -d;
