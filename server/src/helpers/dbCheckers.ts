6e84fc3f681c4badd3c762f62d846b5ed29fad80
Clara Martin <clara@studiometa.fr>
2023-03-30 15:25:16 +0200

    Add the User CRU

--- server/src/helpers/dbCheckers.ts
+++ server/src/helpers/dbCheckers.ts
@@ -2,85 +2,101 @@ import datasource from "../db";
 import { ApolloError } from "apollo-server-errors";
 import City, { CityInput, CityUpdate } from "../entity/City";
 import Place, { PlaceInput } from "../entity/Place";
+import User, { UserInput, UserUpdate } from "../entity/User";
 
 /**
  * Thoses helpers are meant to avoid double entries in database
  */
 
 export const existingCity = async (
-	data: CityInput | CityUpdate,
-	id?: string | undefined
+  data: CityInput | CityUpdate,
+  id?: string | undefined
 ): Promise<void> => {
-	const nameExists = await datasource
-		.getRepository(City)
-		.findOne({ where: { name: data.name } });
+  const nameExists = await datasource
+    .getRepository(City)
+    .findOne({ where: { name: data.name } });
 
-	if (id !== undefined) {
-		// test for modification
-		if (nameExists !== null && nameExists.id !== parseInt(id, 10))
-			throw new ApolloError(
-				"City name already found in database (modification)",
-				"BAD_USER_INPUT"
-			);
-	} else {
-		// test for creation
-		if (nameExists !== null)
-			throw new ApolloError(
-				"City name already found in database (creation)",
-				"BAD_USER_INPUT"
-			);
-	}
+  if (id !== undefined) {
+    // test for modification
+    if (nameExists !== null && nameExists.id !== parseInt(id, 10))
+      throw new ApolloError(
+        "City name already found in database (modification)",
+        "BAD_USER_INPUT"
+      );
+  } else {
+    // test for creation
+    if (nameExists !== null)
+      throw new ApolloError(
+        "City name already found in database (creation)",
+        "BAD_USER_INPUT"
+      );
+  }
+};
+
+export const existingUser = async (
+  data: UserInput | UserUpdate
+): Promise<void> => {
+  const emailExist = await datasource
+    .getRepository(User)
+    .findOne({ where: { email: data.email } });
+
+  // test for creation
+  if (emailExist !== null)
+    throw new ApolloError(
+      "User email already found in database",
+      "BAD_USER_INPUT"
+    );
 };
 
 export const existingCoordinates = async (
-	data: CityInput | CityUpdate
+  data: CityInput | CityUpdate
 ): Promise<void> => {
-	const coordo = await datasource.getRepository(City).findOne({
-		where: { latitude: data.latitude, longitude: data.longitude },
-	});
+  const coordo = await datasource.getRepository(City).findOne({
+    where: { latitude: data.latitude, longitude: data.longitude },
+  });
 
-	if (coordo !== null)
-		throw new ApolloError(
-			"City coordinates found in database",
-			"BAD_USER_INPUT"
-		);
+  if (coordo !== null)
+    throw new ApolloError(
+      "City coordinates found in database",
+      "BAD_USER_INPUT"
+    );
 };
 
 export const existingPlace = async (
-	data: PlaceInput,
-	id?: string | undefined
+  data: PlaceInput,
+  id?: string | undefined
 ): Promise<void> => {
-	const nameExists = await datasource
-		.getRepository(Place)
-		.findOne({ where: { name: data.name } });
+  const nameExists = await datasource
+    .getRepository(Place)
+    .findOne({ where: { name: data.name } });
 
-	if (id !== undefined) {
-		// test for modification
-		if (nameExists !== null && nameExists.id !== parseInt(id, 10))
-			throw new ApolloError(
-				"Place name already found in database (modification)",
-				"BAD_USER_INPUT"
-			);
-	} else {
-		// test for creation
-		if (nameExists !== null)
-			throw new ApolloError(
-				"Place name already found in database (creation)",
-				"BAD_USER_INPUT"
-			);
-	}
+  if (id !== undefined) {
+    // test for modification
+    if (nameExists !== null && nameExists.id !== parseInt(id, 10))
+      throw new ApolloError(
+        "Place name already found in database (modification)",
+        "BAD_USER_INPUT"
+      );
+  } else {
+    // test for creation
+    if (nameExists !== null)
+      throw new ApolloError(
+        "Place name already found in database (creation)",
+        "BAD_USER_INPUT"
+      );
+  }
 };
 
 export const existingPlaceCoordinates = async (
-	data: PlaceInput
+  data: PlaceInput
 ): Promise<void> => {
-	const coordo = await datasource.getRepository(Place).findOne({
-		where: { latitude: data.latitude, longitude: data.longitude },
-	});
+  const coordo = await datasource.getRepository(Place).findOne({
+    where: { latitude: data.latitude, longitude: data.longitude },
+  });
 
-	if (coordo !== null)
-		throw new ApolloError(
-			"Place coordinates found in database",
-			"BAD_USER_INPUT"
-		);
+  if (coordo !== null)
+    throw new ApolloError(
+      "Place coordinates found in database",
+      "BAD_USER_INPUT"
+    );
 };
---
+++ server/src/resolver/UserResolver.ts
@@ -0,0 +1,70 @@
+import { Arg, Mutation, Resolver, Query } from "type-graphql";
+import { ApolloError } from "apollo-server-errors";
+import User, { UserInput, UserUpdate } from "../entity/User";
+import datasource from "../db";
+import { existingUser } from "../helpers/dbCheckers";
+
+@Resolver(User)
+export class UserResolver {
+  @Mutation(() => User)
+  async createUser(@Arg("data") data: UserInput): Promise<User> {
+    if (data === null)
+      throw new ApolloError("No data in query", "BAD_USER_INPUT");
+    // check if user email is already in database
+    await existingUser(data);
+
+    return await datasource.getRepository(User).save(data);
+  }
+
+  @Mutation(() => User)
+  async updateUser(
+    @Arg("email") emailFind: string,
+    @Arg("data") data: UserUpdate
+  ): Promise<User> {
+    const { firstname, lastname, picture, password, role } = data;
+
+    const userToUpdate = await datasource.getRepository(User).findOne({
+      where: { email: emailFind },
+    });
+    if (userToUpdate === null)
+      throw new ApolloError("User not found", "NOT_FOUND");
+
+    if (lastname !== undefined) {
+      userToUpdate.lastname = lastname;
+    }
+    if (firstname !== undefined) {
+      userToUpdate.firstname = firstname;
+    }
+    if (password !== undefined) {
+      userToUpdate.password = password;
+    }
+    if (picture !== undefined) {
+      userToUpdate.picture = picture;
+    }
+    if (role !== undefined) {
+      userToUpdate.role = role;
+    }
+
+    await datasource.getRepository(User).save(userToUpdate);
+
+    return userToUpdate;
+  }
+
+  @Query(() => [User])
+  async getUsers(): Promise<User[]> {
+    return await datasource
+      .getRepository(User)
+      .find({ relations: { cities: true } });
+  }
+
+  @Query(() => User)
+  async getOneUserbyMail(@Arg("email") email: string): Promise<User> {
+    const userToFind = await datasource.getRepository(User).findOne({
+      where: { email },
+    });
+    if (userToFind === null)
+      throw new ApolloError("user not found", "NOT_FOUND");
+
+    return userToFind;
+  }
+}
