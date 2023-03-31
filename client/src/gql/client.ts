13e242661a97b64dd642ddd20c9556fb40ed7655
Elodie Daubie <elodie.daubie@gmail.com>
2023-03-31 11:58:09 +0200

    🚀 change db uri

--- client/src/gql/client.ts
+++ client/src/gql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export default new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "",
   cache: new InMemoryCache(),
   defaultOptions: {
     query: {
