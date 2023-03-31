bd0eed4a5a7e93b771a272e5d72c59a03e684449
Elodie Daubie <elodie.daubie@gmail.com>
2023-03-31 15:23:41 +0200

    change API variable

--- client/src/gql/client.ts
+++ client/src/gql/client.ts
@@ -1,10 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export default new ApolloClient({
-  uri:
-    typeof process.env.REACT_APP_GRAPHQL_API_URL === "undefined"
-      ? "http://localhost:4000"
-      : process.env.REACT_APP_GRAPHQL_API_URL,
+  uri: process.env.REACT_APP_GRAPHQL_API_URL,
   cache: new InMemoryCache(),
   defaultOptions: {
     query: {
