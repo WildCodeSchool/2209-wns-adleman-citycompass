c51e98028fa2d0a64a87e0c8cf5209a1895617fb
Elodie Daubie <elodie.daubie@gmail.com>
2023-05-01 14:50:11 +0200

    ♻️ add dirname to get all resolvers files added in resolver files in schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,11 +8,7 @@ import { env } from "./env";
 import http from "http";
 import { buildSchema } from "type-graphql";
 import datasource from "./db";
-import { CityResolver } from "./resolver/CityResolver";
-import { CategoryResolver } from "./resolver/CategoryResolver";
-import { PlaceResolver } from "./resolver/PlaceResolver";
-import { SearchResolver } from "./resolver/SearchResolver";
-import { UserResolver } from "./resolver/UserResolver";
+import { join } from "path";
 import cors from "cors";
 import User from "./entity/User";
 import jwt from "jsonwebtoken";
@@ -31,13 +27,7 @@ async function start(): Promise<void> {
   const httpServer = http.createServer(app);
 
   const schema = await buildSchema({
-    resolvers: [
-      CategoryResolver,
-      CityResolver,
-      PlaceResolver,
-      SearchResolver,
-      UserResolver,
-    ],
+    resolvers: [join(__dirname, "/resolver/*.{js,ts}")],
     authChecker: async ({ context }: { context: ContextType }, roles = []) => {
       const { req } = context;
       const tokenInHeaders = req.headers.authorization?.split(" ")[1];
