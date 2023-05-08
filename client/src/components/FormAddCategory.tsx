3ed08161931e9a03cd314d7786b9d116f1ba0dd9
Elodie Daubie <elodie.daubie@gmail.com>
2023-05-08 15:03:16 +0200

    📦 install formik and implement it to add category form

--- client/src/components/FormAddCategory.tsx
+++ client/src/components/FormAddCategory.tsx
@@ -1,45 +1,57 @@
-import React, { FormEvent, useState } from "react";
+import React from "react";
+import { useFormik } from "formik";
 import { useCreateCategoryMutation } from "../gql/generated/schema";
 
-function FormAddCategory() {
-  const [name, setName] = useState("");
-  const [picto, setPicto] = useState("");
+export default function FormAddCategory() {
   const [createCategory] = useCreateCategoryMutation();
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    await createCategory({
-      variables: {
-        data: { name, picto },
-      },
-    });
-  };
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      picto: "",
+    },
+    onSubmit: (values) => {
+      createCategory({
+        variables: {
+          data: { name: values.name, picto: values.picto },
+        },
+      });
+    },
+  });
 
   return (
-    <form onSubmit={handleSubmit} className="container flex flex-col w-50">
-      <label htmlFor="name" className="modal__input--label">
-        Nom
-        <input
-          type="text"
-          name="name"
-          id="name"
-          className="modal__input shadow shadow-green mb-4"
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      <label htmlFor="picto" className="modal__input--label">
-        Pictogramme
-        <input
-          type="url"
-          name="picto"
-          id="picto"
-          className="modal__input shadow shadow-green mb-4"
-          onChange={(e) => setPicto(e.target.value)}
-        />
-      </label>
-      <input type="submit" className="button--primary mt-6" />
-    </form>
+    <div>
+      <h1>Titre du formulaire</h1>
+      <form
+        onSubmit={formik.handleSubmit}
+        className="container flex flex-col w-50"
+      >
+        <label htmlFor="name" className="modal__input--label">
+          Nom
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="modal__input shadow shadow-green mb-4"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+        </label>
+        <label htmlFor="picto" className="modal__input--label">
+          Pictogramme
+          <input
+            type="url"
+            name="picto"
+            id="picto"
+            className="modal__input shadow shadow-green mb-4"
+            onChange={formik.handleChange}
+            value={formik.values.picto}
+          />
+        </label>
+        <button type="submit" className="button--primary mt-6">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
-
-export default FormAddCategory;
