bb21785b596ce5b77bb598b57cd3dc3a1ac42ee7
Elodie Daubie <elodie.daubie@gmail.com>
2023-05-08 16:44:26 +0200

    🎨 implement Form component from Formik to improve errors rendering

--- client/src/components/FormAddCategory.tsx
+++ client/src/components/FormAddCategory.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useFormik } from "formik";
+import { useFormik, ErrorMessage, Formik, Field, Form } from "formik";
 import { useCreateCategoryMutation } from "../gql/generated/schema";
 
 interface FormAddCategoryType {
@@ -14,82 +14,76 @@ interface FormAddCategoryErrorsType {
 
 // validations
 
-const validate = (values: FormAddCategoryType) => {
-  const errors: FormAddCategoryErrorsType = {};
-  if (!values.name) {
-    errors.name = "Le nom est obligatoire";
-  } else if (values.name.length < 2) {
-    errors.name = "Le nom doit avoir au moins 2 caractères";
-  } else if (values.name.trim() === "") {
-    errors.name = "Le nom est invalide";
+function validateName(name: string) {
+  let error;
+  if (!name) {
+    error = "Le nom est obligatoire";
+  } else if (name.length < 2) {
+    error = "Le nom doit avoir au moins 2 caractères";
+  } else if (name.trim() === "") {
+    error = "Le nom est invalide";
   }
+  return error;
+}
 
-  if (!values.picto) {
-    errors.picto = "Le pictogramme est obligatoire";
-  } else if (!/^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i.test(values.picto)) {
-    errors.picto = "Le pictogramme doit être une URL";
+function validatePicto(picto: string) {
+  let error;
+  if (!picto) {
+    error = "Le pictogramme est obligatoire";
+  } else if (!/^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i.test(picto)) {
+    picto = "Le pictogramme doit être une URL";
   }
-  return errors;
-};
+  return error;
+}
 
-// form building
+// form building with Formik https://formik.org/docs/guides/validation
 
 export default function FormAddCategory() {
   const [createCategory] = useCreateCategoryMutation();
 
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      picto: "",
-    },
-    validate,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-      createCategory({
-        variables: {
-          data: { name: values.name, picto: values.picto },
-        },
-      });
-    },
-  });
-
   return (
     <div>
       <h1>Ajouter une catégorie</h1>
-      <form
-        onSubmit={formik.handleSubmit}
-        className="container flex flex-col w-50"
+      <Formik
+        initialValues={{
+          name: "",
+          picto: "",
+        }}
+        onSubmit={(values) => {
+          createCategory({
+            variables: {
+              data: { name: values.name, picto: values.picto },
+            },
+          });
+          alert(JSON.stringify(values, null, 2));
+        }}
       >
-        <label htmlFor="name" className="modal__input--label"></label>
-        Nom
-        <input
-          type="text"
-          name="name"
-          id="name"
-          className="modal__input shadow shadow-green mb-4"
-          onChange={formik.handleChange}
-          value={formik.values.name}
-        />
-        {formik.errors.name ? (
-          <div className="text-red">{formik.errors.name}</div>
-        ) : null}
-        <label htmlFor="picto" className="modal__input--label"></label>
-        Pictogramme
-        <input
-          type="url"
-          name="picto"
-          id="picto"
-          className="modal__input shadow shadow-green mb-4"
-          onChange={formik.handleChange}
-          value={formik.values.picto}
-        />
-        {formik.errors.picto ? (
-          <div className="text-red">{formik.errors.picto}</div>
-        ) : null}
-        <button type="submit" className="button--primary mt-6">
-          Enregistrer
-        </button>
-      </form>
+        {({ errors, touched, isValidating }) => (
+          <Form className="container flex flex-col w-1/2">
+            <Field
+              name="name"
+              validate={validateName}
+              placeholder="nom"
+              className="modal__input shadow shadow-green mb-4"
+            />
+            {errors.name && touched.name && (
+              <div className="text-red">{errors.name}</div>
+            )}
+            <Field
+              name="picto"
+              validate={validatePicto}
+              placeholder="https://mon-pictogramme.net"
+              className="modal__input shadow shadow-green mb-4"
+            />
+            {errors.picto && touched.picto && (
+              <div className="text-red">{errors.picto}</div>
+            )}
+            <button type="submit" className="button--primary mt-6">
+              Enregistrer
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
