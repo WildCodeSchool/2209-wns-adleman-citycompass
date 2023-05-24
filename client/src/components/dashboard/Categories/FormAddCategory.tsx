afb6d996c0d26b0bd1c0e608db63f98341054aeb
Elodie Daubie <elodie.daubie@gmail.com>
2023-05-24 11:46:02 +0200

    🥅 add tiast to display erros differents from category already exists

--- client/src/components/dashboard/Categories/FormAddCategory.tsx
+++ client/src/components/dashboard/Categories/FormAddCategory.tsx
@@ -63,6 +63,8 @@ export default function FormAddCategory({
         res.errors.forEach(({ message }) => {
           if ((message = "Category Already exists")) {
             toast.error("La catégorie existe déjà");
+          } else {
+            toast.error(message);
           }
         });
       }
