26222ea18ea86c46e28e8ddac72abdd2c9f45b57
Clara Martin <clara@studiometa.fr>
2023-05-25 12:24:12 +0200

    Refactorise code and validation

--- client/src/components/dashboard/Cities/FormAddCity.tsx
+++ client/src/components/dashboard/Cities/FormAddCity.tsx
@@ -6,66 +6,17 @@ import {
   GetCitiesDocument,
 } from "../../../gql/generated/schema";
 import { toast } from "react-hot-toast";
+import {
+  validateDescription,
+  validateLatitude,
+  validateLongitude,
+  validateName,
+  validatePicture,
+} from "../../../utils/formValidator";
 
 interface FormAddCityProps {
   setListCities: React.Dispatch<React.SetStateAction<boolean>>;
   setAddCities: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-// validations
-
-function validateName(name: string) {
-  let error;
-  if (!name) {
-    error = "Le nom est obligatoire";
-  } else if (name.length < 2) {
-    error = "Le nom doit avoir au moins 2 caractères";
-  } else if (name.trim() === "") {
-    error = "Le nom est invalide";
-  }
-  return error;
-}
-
-function validatePicture(picture: string) {
-  let error;
-  if (!picture) {
-    error = "L'image est obligatoire";
-  } else if (!/^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i.test(picture)) {
-    error = "L'image doit être une URL";
-  }
-  return error;
-}
-
-function validateDescription(description: string) {
-  let error;
-  if (!description) {
-    error = "La description est obligatoire";
-  } else if (description.length < 10) {
-    error = "La description doit faire au moins 10 caractères";
-  }
-  return error;
-}
-
-function validateLatitude(latitude: string) {
-  let error;
-  if (!latitude) {
-    error = "La latitude est obligatoire";
-  } else if (!/^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$/.test(latitude)) {
-    error = "La donnée doit être une latitude";
-  }
-  return error;
-}
-
-function validateLongitude(longitude: string) {
-  let error;
-  if (!longitude) {
-    error = "La longitude est obligatoire";
-  } else if (
-    !/^[-+]?([1-9]?\d(\.\d+)?|1[0-7]\d(\.\d+)?|180(\.0+)?)$/.test(longitude)
-  ) {
-    error = "La donnée doit être une longitude";
-  }
-  return error;
 }
 
 // form building with Formik https://formik.org/docs/guides/validation
