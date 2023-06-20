a8a48d87484e9a3a2af77cb541403b5ab742ee4d
Elodie Daubie <elodie.daubie@gmail.com>
2023-06-20 10:45:18 +0200

    ✨ Add role to UserInformations component and changed title to re-use it easily

--- client/src/components/dashboard/Accueil/UserInformations.tsx
+++ client/src/components/dashboard/Accueil/UserInformations.tsx
@@ -10,13 +10,14 @@ interface FormUpdateUserProps {
 export function UserInformations({ user }: FormUpdateUserProps) {
   return (
     <div className="container py-6 bg-cream flex flex-col w-fit">
-      <h3 className="type-h3">Données personnelles</h3>
+      <h3 className="type-h3">Informations de profil</h3>
       <div className="my-6">
         <ul className="flex flex-col gap-8 ml-5">
           <li className="list-disc">Nom : {user.lastname}</li>
           <li className="list-disc">Prénom : {user.firstname}</li>
           <li className="list-disc">Avatar : {user.picture}</li>
           <li className="list-disc">Email : {user.email}</li>
+          <li className="list-disc">Role: {user.role}</li>
         </ul>
       </div>
     </div>
