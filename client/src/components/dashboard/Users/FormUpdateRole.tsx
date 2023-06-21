12eb60b38546038a556bf31ea3f8357bf9d4d035
Elodie Daubie <elodie.daubie@gmail.com>
2023-06-21 14:39:26 +0200

    💄 add conditionnal display : if user is admin he cannot update admin and superadmin roles

--- client/src/components/dashboard/Users/FormUpdateRole.tsx
+++ client/src/components/dashboard/Users/FormUpdateRole.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Formik, Field, Form } from "formik";
 import { userToUpdateProps } from "./UsersDashboard";
 import { UserInformations } from "../Accueil/UserInformations";
-import { UserRoleUpdate } from "../../../gql/generated/schema";
+import {
+  User,
+  UserRoleUpdate,
+  useGetProfileQuery,
+} from "../../../gql/generated/schema";
 import { useUpdateUserRoleMutation } from "../../../gql/generated/schema";
 import { toast } from "react-hot-toast";
 import { roles } from "../../../utils/userRoles";
@@ -19,6 +23,13 @@ export function FormUpdateRole({
   userToUpdate,
 }: FormUpdateRoleProps) {
   const [updateRole] = useUpdateUserRoleMutation({ errorPolicy: "all" });
+
+  const { data } = useGetProfileQuery({
+    errorPolicy: "ignore",
+  });
+
+  const currentUser = data?.profile;
+
   const handleSubmit = async (values: UserRoleUpdate) => {
     updateRole({
       variables: {
@@ -36,57 +47,89 @@ export function FormUpdateRole({
     });
   };
 
+  const isAuthorized = (currentUser: any, userToUpdate: User): boolean => {
+    if (currentUser.role === "superadmin") return true;
+    console.log(currentUser);
+    if (
+      currentUser.role === "admin" &&
+      (userToUpdate.role === "contributor" || userToUpdate.role === "visitor")
+    )
+      return true;
+    return false;
+  };
+
   return (
-    <div className="container mx-auto p-6 bg-cream flex flex-col">
-      <UserInformations user={userToUpdate} />
-      <h3 className="type-h4 text-center py-6">
-        Changer le role de l'utilisateur
-      </h3>
-      <Formik
-        initialValues={{ role: userToUpdate.role }}
-        onSubmit={handleSubmit}
-      >
-        {({ values }) => (
-          <Form className="flex flex-col gap-4 my-8">
-            <div
-              role="group"
-              aria-labelledby="my-radio-group"
-              className="flex flex-row gap-4 justify-center align-center"
+    <>
+      <div className="container mx-auto p-6 bg-cream flex flex-col">
+        <UserInformations user={userToUpdate} />
+        {isAuthorized(currentUser, userToUpdate) ? (
+          <>
+            <h3 className="type-h4 text-center py-6">
+              Changer le role de l'utilisateur
+            </h3>
+            <Formik
+              initialValues={{ role: userToUpdate.role }}
+              onSubmit={handleSubmit}
             >
-              {roles?.map((e) => (
-                <label
-                  htmlFor={`${e}`}
-                  className="flex gap-3 py-4 px-6 border-2 border-gray rounded
+              {({ values }) => (
+                <Form className="flex flex-col gap-4 my-8">
+                  <div
+                    role="group"
+                    aria-labelledby="my-radio-group"
+                    className="flex flex-row gap-4 justify-center align-center"
+                  >
+                    {roles?.map((e) => (
+                      <label
+                        htmlFor={`${e}`}
+                        className="flex gap-3 py-4 px-6 border-2 border-gray rounded
                 bg-white cursor-pointer hover:bg-orange"
-                >
-                  <Field
-                    type="radio"
-                    name="role"
-                    value={`${e}`}
-                    id={`${e}`}
-                  ></Field>
-                  {e}
-                </label>
-              ))}
-            </div>
-            <button
-              type="submit"
-              className="button--primary mt-6 w-1/4 self-center"
-            >
-              Enregistrer
-            </button>
+                      >
+                        <Field
+                          type="radio"
+                          name="role"
+                          value={`${e}`}
+                          id={`${e}`}
+                        ></Field>
+                        {e}
+                      </label>
+                    ))}
+                  </div>
+                  <button
+                    type="submit"
+                    className="button--primary mt-6 w-1/4 self-center"
+                  >
+                    Enregistrer
+                  </button>
+                  <div
+                    className="modal__input--label text-s mt-3 text-center cursor-pointer"
+                    onClick={() => {
+                      setListUsers(true);
+                      setModifyUsers(false);
+                    }}
+                  >
+                    Annuler
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </>
+        ) : (
+          <div>
+            <p className="text-center font-semibold text-red ">
+              Seuls les superadmin peuvent gérer les droits de cet utilisateur
+            </p>
             <div
-              className="modal__input--label text-s mt-3 text-center cursor-pointer"
+              className="button--primary w-1/4 text-center mx-auto my-6"
               onClick={() => {
                 setListUsers(true);
                 setModifyUsers(false);
               }}
             >
-              Annuler
+              Retour à la liste
             </div>
-          </Form>
+          </div>
         )}
-      </Formik>
-    </div>
+      </div>
+    </>
   );
 }
