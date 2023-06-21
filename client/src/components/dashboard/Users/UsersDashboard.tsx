8e58a46de525c4ed708effed3463367e167205c4
Elodie Daubie <elodie.daubie@gmail.com>
2023-06-21 10:37:46 +0200

    ✨ Filter users by role

--- client/src/components/dashboard/Users/UsersDashboard.tsx
+++ client/src/components/dashboard/Users/UsersDashboard.tsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import modify_icon from "../../../assets/modify_icon.svg";
 import { useGetUsersQuery } from "../../../gql/generated/schema";
 import { FormUpdateRole } from "./FormUpdateRole";
+import { roles } from "../../../utils/userRoles";
+import chevron_down from "../../../assets/chevron-arrow-down.png";
+import chevron_up from "../../../assets/up-arrow-angle.png";
 
 export interface userToUpdateProps {
   id: number;
@@ -26,9 +29,10 @@ function UsersDashboard() {
     picture: "",
     role: "",
   });
+  const [userList, setUserList] = useState(false);
+  const [roleToDisplay, setRoleToDisplay] = useState("");
 
   const { data } = useGetUsersQuery();
-
   const users = data?.getUsers;
 
   return (
@@ -42,22 +46,58 @@ function UsersDashboard() {
         <div className="bg-cream w-full h-fit min-h-[75%] mt-10">
           {listUsers && (
             <div className="flex flex-col justify-center w-full h-full gap-4 pb-8">
-              {users?.map((user) => (
-                <div
-                  className="h-12 w-96 px-6 self-center rounded bg-orange flex justify-between items-center"
-                  key={user.id}
-                >
-                  <p className="w-4/5">{user.firstname}</p>
-                  <button
-                    onClick={() => (
-                      setModifyUsers(true),
-                      setListUsers(false),
-                      setUserToUpdate(user)
-                    )}
-                  >
-                    <img src={modify_icon} alt="" className="w-6" />
-                  </button>
-                </div>
+              {roles.map((role) => (
+                <>
+                  {
+                    <div
+                      className="h-12 w-96 px-6 self-center rounded bg-orange flex justify-between items-center"
+                      key={role}
+                    >
+                      <p className="w-4/5">{role}</p>
+                      <button
+                        onClick={() => {
+                          setRoleToDisplay(role);
+                          setUserList(!userList);
+                        }}
+                      >
+                        <img
+                          src={
+                            userList && role === roleToDisplay
+                              ? chevron_up
+                              : chevron_down
+                          }
+                          alt=""
+                          className="w-4"
+                        />
+                      </button>
+                    </div>
+                  }
+                  <div className="flex flex-col justify-between self-center">
+                    {userList &&
+                      role === roleToDisplay &&
+                      users
+                        ?.filter((user) => user.role === role)
+                        .map((user) => (
+                          <div
+                            className="h-12 w-96 px-6 flex justify-between items-center"
+                            key={user.id}
+                          >
+                            <p className="w-4/5">
+                              {user.firstname} {user.lastname}
+                            </p>
+                            <button
+                              onClick={() => (
+                                setModifyUsers(true),
+                                setListUsers(false),
+                                setUserToUpdate(user)
+                              )}
+                            >
+                              <img src={modify_icon} alt="" className="w-6" />
+                            </button>
+                          </div>
+                        ))}
+                  </div>
+                </>
               ))}
             </div>
           )}
