abb8989e09cbe1f8ce22ba2b34c395af362a9fd9
Alexandre Pillias <alexandre.pillias@gmail.com>
2023-06-22 14:58:26 +0200

    🔥 remove commentary

--- server/src/entity/Place.ts
+++ server/src/entity/Place.ts
@@ -64,9 +64,6 @@ class Place {
     eager: true,
   })
   author?: User;
-
-  // to do : add a many to one relation with user, and name the property "author"
-  // a contributor can modify a place only if he is the author (but city admin can modify them all)
 }
 
 @InputType()
