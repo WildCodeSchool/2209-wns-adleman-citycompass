6796153da6ecef3eaeb837f0a9815515b37a6a7c
Clara Martin <clara@studiometa.fr>
2023-07-19 15:32:36 +0200

    Add hover effects

--- client/src/components/MiniCardPoi.tsx
+++ client/src/components/MiniCardPoi.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { PlaceProps } from "./MapCity";
+import { useNavigate } from "react-router-dom";
+import { heroContent } from "../components/Hero";
 
 interface cardProps {
   place: PlaceProps | undefined;
+  city: heroContent;
 }
 
-function MiniCardPoi({ place }: cardProps) {
+function MiniCardPoi({ place, city }: cardProps) {
+  const navigate = useNavigate();
   return (
     <>
       {place && (
-        <div className="w-full bg-white drop-shadow-green border-green border-2 overflow-hidden rounded flex">
+        <div
+          className="w-full bg-white drop-shadow-green border-green hover:border-green-dark transition border-2 overflow-hidden rounded flex cursor-pointer"
+          onClick={() =>
+            navigate(`/cities/${city.name}/${place.name}`, {
+              state: { place },
+            })
+          }
+        >
           <div className="w-1/3 h-28 overflow-hidden">
             <img
               src={place.picture}
